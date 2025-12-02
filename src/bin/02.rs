a5ea64997e24a317923530b4a9896b6737b730d6
Fgdou <fabigoardou@gmail.com>
2025-12-02 17:40:23 +0100

    day02 part2

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,5 +1,5 @@
 #![feature(new_range_api)]
-use std::range::{Range, RangeInclusive};
+use std::{collections::HashSet, range::RangeInclusive};
 
 advent_of_code::solution!(2);
 
@@ -26,7 +26,23 @@ pub fn part_one(input: &str) -> Option<u64> {
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let input = parse(input);
+
+
+    let sum = input.iter().map(|range|
+        range.iter()
+            .filter_map(|n|
+                if check_double_or_more(n) {
+                    Some(n as u64)
+                } else {
+                    None
+                }
+            )
+            .sum::<u64>()
+    )
+    .sum();
+
+    return Some(sum);
 }
 
 fn check_double(n: u64) -> bool {
@@ -53,6 +69,45 @@ fn parse(input: &str) -> Input {
         .collect()
 }
 
+fn all_element_same(list: Vec<&str>) -> bool {
+    let set: HashSet<&str> = HashSet::from_iter(list.iter().cloned());
+
+    set.len() == 1
+}
+
+fn check_double_or_more(n: u64) -> bool {
+    let txt = n.to_string();
+
+    for i in 2..=txt.len() {
+        let list = split_by(&txt, i as usize);
+
+        let all_same = match list {
+            None => continue,
+            Some(vec) => all_element_same(vec)
+        };
+
+        if all_same {
+            return true
+        }
+    };
+
+    false
+}
+
+fn split_by(txt: &str, n: usize) -> Option<Vec<&str>> {
+    if txt.len() % n != 0 {
+        return None;
+    }
+
+    let delta = txt.len() / n;
+
+    let res = (0..n).into_iter()
+        .map(|i| &txt[delta*i..delta*(i+1)])
+        .collect();
+
+    Some(res)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -66,7 +121,7 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(4174379265));
     }
 
     #[test]
@@ -78,5 +133,23 @@ mod tests {
         assert_eq!(true, check_double(123123));
         assert_eq!(false, check_double(1230123));
         assert_eq!(true, check_double(1188511885))
+    }
+
+    #[test]
+    fn test_split_by() {
+        assert_eq!(Some(vec!("")), split_by("", 1));
+        assert_eq!(Some(vec!("123", "123")), split_by("123123", 2));
+        assert_eq!(Some(vec!("123", "123", "123")), split_by("123123123", 3));
+        assert_eq!(Some(vec!("12", "31", "23")), split_by("123123", 3));
+        assert_eq!(None, split_by("123123", 4));
+    }
+
+    #[test]
+    fn test_more_than_double() {
+        assert_eq!(false, check_double_or_more(123));
+        assert_eq!(true, check_double_or_more(1212));
+        assert_eq!(true, check_double_or_more(11));
+        assert_eq!(true, check_double_or_more(1111));
+        assert_eq!(true, check_double_or_more(123123123));
     }
 }
