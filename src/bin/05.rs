a8d9f2d887fb8061f26ae51ee9a45d8b936da94a
Fgdou <fabigoardou@gmail.com>
2025-12-06 11:01:20 +0100

    day05 part2

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -1,4 +1,7 @@
-use std::ops::RangeInclusive;
+#![feature(get_disjoint_mut_helpers)]
+
+use core::slice::GetDisjointMutIndex;
+use std::{collections::HashSet, ops::RangeInclusive};
 
 advent_of_code::solution!(5);
 
@@ -17,12 +20,39 @@ pub fn part_one(input: &str) -> Option<usize> {
     Some(ingredients.len())
 }
 
-pub fn part_two(input: &str) -> Option<u64> {
-    None
+pub fn part_two(input: &str) -> Option<usize> {
+    let input = parse(input);
+
+    let mut ranges = input.range_list;
+    ranges.sort_by_key(|obj| *obj.start());
+
+    let mut simplified: Vec<RangeInclusive<usize>> = vec!();
+
+    for range in ranges {
+        if simplified.is_empty() {
+            simplified.push(range.clone());
+            continue;
+        }
+        let first = simplified.remove(simplified.len()-1);
+        simplify(&first, &range).into_iter()
+            .for_each(|i| simplified.push(i));
+    }
+
+    let res = simplified.iter().map(|range| range.end() - range.start() + 1).sum();
+
+    Some(res)
 }
 
 fn in_range(ingredient: usize, range_list: &Vec<RangeInclusive<usize>>) -> bool {
     range_list.iter().any(|range| range.contains(&ingredient))
+}
+
+fn simplify(first: &RangeInclusive<usize>, second: &RangeInclusive<usize>) -> Vec<RangeInclusive<usize>> {
+    if first.is_overlapping(second) {
+        vec!(usize::min(*first.start(), *second.start())..=usize::max(*first.end(), *second.end()))
+    } else {
+        vec!(first.clone(), second.clone())
+    }
 }
 
 fn parse(input: &str) -> Input {
@@ -58,6 +88,13 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(14));
+    }
+
+    #[test]
+    fn test_simplify() {
+        assert_eq!(vec!(0..=1, 2..=3), simplify(&(0..=1), &(2..=3)));
+        assert_eq!(vec!(0..=3), simplify(&(0..=2), &(2..=3)));
+        assert_eq!(vec!(0..=9), simplify(&(0..=9), &(2..=3)));
     }
 }
