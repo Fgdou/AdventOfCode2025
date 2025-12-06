828d7d57c288a7ed779dbd9e8300b092c1a72902
Fgdou <fabigoardou@gmail.com>
2025-12-06 11:47:55 +0100

    day06 part2

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -6,6 +6,8 @@ struct Input {
     operations: Vec<char>,
 }
 
+type Input2 = Vec<Vec<char>>;
+
 type MathTable = Vec<Operation>;
 
 #[derive(Debug)]
@@ -23,8 +25,13 @@ pub fn part_one(input: &str) -> Option<usize> {
     Some(res)
 }
 
-pub fn part_two(input: &str) -> Option<u64> {
-    None
+pub fn part_two(input: &str) -> Option<usize> {
+    let input = parse2(input);
+    let table = get_table(&input);
+
+    let res = table.iter().map(|operation| calculate(operation)).sum();
+
+    Some(res)
 }
 
 fn transform_to_table(input: &Input) -> MathTable {
@@ -74,6 +81,51 @@ fn parse(input: &str) -> Input {
     }
 }
 
+fn get_table(input: &Input2) -> MathTable {
+    let operation_line: Vec<char> = input[input.len()-1].iter().cloned().collect();
+    let numbers: Vec<Vec<char>> = input[0..input.len()-1].iter().cloned().collect();
+
+    let operations: Vec<_> = operation_line.iter()
+        .enumerate()
+        .filter(|(_, c)| **c != ' ')
+        .collect();
+
+    let mut table = vec!();
+
+    for i in 0..operations.len() {
+        let left_index = operations.get(i).unwrap().0;
+        let right_index = match operations.get(i+1) {
+            None => operation_line.len(),
+            Some((i, _)) => *i,
+        };
+
+        let c = *operations.get(i).unwrap().1;
+
+        let numbers: Vec<usize> = (left_index..right_index).into_iter().filter_map(|x| {
+            let numbers_str: String = (0..numbers.len()).into_iter().map(|y| {
+                *numbers.get(y).unwrap().get(x).unwrap()
+            }).collect();
+            if numbers_str.trim().is_empty() {
+                None
+            } else {
+                Some(numbers_str.trim().parse().unwrap())
+            }
+        }).collect();
+
+        table.push(Operation { operation: c, numbers });
+
+    }
+
+    table
+}
+
+fn parse2(input: &str) -> Input2 {
+    input.split('\n')
+        .filter(|line| !line.is_empty())
+        .map(|line| line.chars().collect())
+        .collect()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -87,6 +139,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(3263827));
     }
 }
