f31cd24aea69e7fd12524602edf7cab7b09faf89
Fgdou <fabigoardou@gmail.com>
2025-12-08 10:43:48 +0100

    day07 part2

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -43,8 +43,56 @@ pub fn part_one(input: &str) -> Option<usize> {
     Some(splitters.len())
 }
 
+fn get_ends(input: &Input) -> HashSet<Vector2<usize>> {
+    let mut stack = vec!(input.start);
+    let mut end = HashSet::new();
+    let mut visited = HashSet::new();
+
+    while !stack.is_empty() {
+        let pos = stack.pop().unwrap();
+        if visited.contains(&pos) {
+            continue;
+        }
+        visited.insert(pos.clone());
+
+        if pos[1] >= input.height {
+            end.insert(pos);
+        } else {
+            let splitter = move_ray(pos, &input.splitters);
+            splitter.rays.into_iter().for_each(|ray| stack.push(ray));
+        }
+    }
+
+    end
+}
+
+fn visit(pos: Vector2<usize>, visited: &mut HashMap<Vector2<usize>, usize>, splitters: &HashSet<Vector2<usize>>, height: usize) -> usize {
+    let pos = [pos[0], pos[1]+1];
+
+    if let Some(value) = visited.get(&pos) {
+        return *value;
+    }
+
+    let sum = if pos[1] >= height {
+        1
+    } else if splitters.contains(&pos) {
+        visit([pos[0]-1, pos[1]], visited, splitters, height) + visit([pos[0]+1, pos[1]], visited, splitters, height)
+    } else {
+        visit(pos, visited, splitters, height)
+    };
+
+    visited.insert(pos, sum);
+
+    sum
+}
+
 pub fn part_two(input: &str) -> Option<usize> {
-    None
+    let input = parse(input);
+    let mut visited: HashMap<Vector2<usize>, usize> = HashMap::new();
+
+    let res = visit(input.start, &mut visited, &input.splitters, input.height);
+
+    Some(res)
 }
 
 fn parse(input: &str) -> Input {
@@ -96,7 +144,7 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(40));
     }
 
     #[test]
