9f73c77e3f93ae627ff189ceb27fd386a0c537ff
Fgdou <fabigoardou@gmail.com>
2025-12-10 19:36:11 +0100

    day10 part2

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -2,7 +2,7 @@
 
 use std::{collections::{HashSet, LinkedList}, rc::Rc};
 
-use regex::bytes::Regex;
+use z3::{Optimize, SatResult, Solver, ast::Int};
 
 advent_of_code::solution!(10);
 
@@ -17,8 +17,15 @@ pub fn part_one(input: &str) -> Option<usize> {
     Some(sum)
 }
 
-pub fn part_two(input: &str) -> Option<u64> {
-    None
+pub fn part_two(input: &str) -> Option<usize> {
+    let input = parse(input);
+
+    let sum = input.iter().enumerate().map(|(i, machine)| {
+        println!("{}/{}", i, input.len());
+        calculate_fewest_joltage(machine)
+    }).sum();
+
+    Some(sum)
 }
 
 struct StackEntry {
@@ -52,13 +59,48 @@ fn calculate_fewest(machine: &Machine) -> usize {
     }
 }
 
+fn calculate_fewest_joltage(machine: &Machine) -> usize {
+    let solver = Optimize::new();
+    let buttons: Vec<_> = machine.buttons.iter().enumerate().map(|(i, n)| Int::new_const(i.to_string())).collect();
+
+    for b in &buttons {
+        solver.assert(&b.ge(0));
+    }
+
+    for (i, joltage) in machine.joltage.iter().enumerate() {
+        let buttons_in_joltage: Vec<_> = machine.buttons.iter().enumerate().filter(|(j, b)| b.contains(&i)).map(|(j, b)| j).collect();
+
+        assert!(buttons_in_joltage.len() != 0);
+
+        let equation: Int = buttons_in_joltage.iter().fold(Int::from(0u64), |acc, i| acc + buttons.get(*i).unwrap());
+
+        solver.assert(&equation.eq(*joltage as u64));
+    }
+
+    let total = buttons.iter().fold(Int::from(0u64), |acc, i| acc + i);
+    solver.minimize(&total);
+
+    let solution: Vec<_> = match solver.check(&[]) {
+        SatResult::Sat => {
+            let model = solver.get_model().unwrap();
+
+            buttons.iter().map(|b| model.eval(b, true).unwrap()).collect()
+        }
+        _ => panic!()
+    };
+
+    let numbers: Vec<_> = solution.iter().map(Int::as_u64).map(Option::unwrap).collect();
+
+    numbers.iter().sum::<u64>() as usize
+}
+
 type Input = Vec<Machine>;
 
 #[derive(Debug, PartialEq, Eq)]
 struct Machine {
     lights: Vec<bool>,
     buttons: Vec<HashSet<usize>>,
-    joltage: HashSet<usize>,
+    joltage: Vec<usize>,
 }
 
 fn apply_button(button: &HashSet<usize>, lights: &Vec<bool>) -> Vec<bool> {
@@ -121,7 +163,7 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(33));
     }
 
     #[test]
@@ -145,7 +187,7 @@ mod tests {
                     HashSet::from([0, 2]),
                     HashSet::from([0, 1]),
                 ),
-                joltage: HashSet::from([3, 5, 4, 7])
+                joltage: vec!(3, 5, 4, 7)
             }),
             parse(&"[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}")
         )
